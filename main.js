0727beb93a2be4bd24aae20c64dd6666ae817070
Robert <robertmdinca@proton.me>
2023-05-06 14:28:55 +0200

    Eliminar tarea agregado, por fin

--- main.js
+++ main.js
@@ -1,34 +1,39 @@
-const input = document.getElementById('task');
-const tasks = document.getElementById('tasks');
+const input = document.getElementById('inputTask');
+const tasks = document.getElementById('SectionTasks');
 const addButton = document.getElementById('add');
-const removeButton = document.getElementsByClassName('remove');
+const removeButtons = document.getElementsByClassName('remove');
+const addButtons = document.getElementsByClassName('task')
+let counter = 1;
+
+
 
 addButton.addEventListener('click', addTask);
-removeButton.addEventListener('click', removeTask);
+
+
 
 function addTask() {
     const taskDiv = document.createElement('div');
     taskDiv.className = 'task';
+    taskDiv.id = 'task' + counter;
 
     const taskLi = document.createElement('li');
     taskLi.innerText = input.value;
 
     const taskButton = document.createElement('button');
-    taskButton.className = 'remove';
+    taskButton.className = 'remove' + counter;
     taskButton.innerText = '-';
+    taskButton.addEventListener('click', function() {
+        const contenedorPadre = this.closest('.task');
+        contenedorPadre.remove();
+      });
 
     taskDiv.appendChild(taskLi);
     taskDiv.appendChild(taskButton);
     tasks.appendChild(taskDiv);
 
     input.value = '';
-    
+    counter++;
 }
 
-function removeTask() {
-    const task = this.parentNode;
-    const tasks = task.parentNode;
-    tasks.removeChild(task);
-}
 
 
